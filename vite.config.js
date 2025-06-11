c62e8f168a02118abff6a4b110f55122650dbf0c
MissCeptstudios <joanna.cholas@gmail.com>
2025-06-11 10:50:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ib-calculator/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
